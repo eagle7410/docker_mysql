572ef7ee98c6408b7ea3356ee87e3537bf568314
Igor <verycooleagle@gmail.com>
2018-02-25 00:28:19 +0200

    Add git ignore

--- init_repo.sh
+++ init_repo.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+echo "# docker_mysql" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:eagle7410/docker_mysql.git
+git push -u origin master
